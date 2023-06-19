b7c4cdb08edadb625f4704fb537f9a41c1368014
rraulison <91005049+rraulison@users.noreply.github.com>
2023-06-19 08:28:10 -0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -11,19 +11,33 @@ download_url=$(echo "${release_info}" | grep "browser_download_url" | grep -oE '
 filename=$(basename "${download_url}")
 
 # Obter o nome do arquivo .AppImage mais recente na pasta de downloads
-arquivo_downloads=$(find "${pasta_downloads}" -maxdepth 1 -name "*.AppImage" -printf "%f\n" 2>/dev/null | sort -V | tail -n 1)
+arquivo_downloads=$(find "${pasta_downloads}" -maxdepth 1 \( -iname "*.AppImage" -o -iname "*.appimage" \)  -printf "%f\n" 2>/dev/null | sort -V | tail -n 1)
 
 # Verificar se existe um arquivo .AppImage na pasta de downloads
 if [[ -z "${arquivo_downloads}" ]]; then
   echo "Nenhum arquivo .AppImage encontrado na pasta ${pasta_downloads}."
+  # Baixar o arquivo .AppImage mais recente
+  echo "Baixando o arquivo .AppImage mais recente..."
+  wget -q --show-progress -P "${pasta_downloads}" "${download_url}"
+  echo "Download concluído!"
 else
   # Comparar os nomes dos arquivos
   if [[ "${filename}" > "${arquivo_downloads}" ]]; then
+    # Delete versoes anteriores
+    echo "Deleting existing .AppImage files..."
+    find "$pasta_downloads" -maxdepth 1 \( -iname "*.AppImage" -o -iname "*.appimage" \) -type f -not -name "${filename}" -delete
+    echo "Deletion complete!"
+
     # Baixar o arquivo .AppImage mais recente
     echo "Baixando o arquivo .AppImage mais recente..."
     wget -q --show-progress -P "${pasta_downloads}" "${download_url}"
     echo "Download concluído!"
   else
     echo "Arquivo mais recente já baixado."
+    # Delete outros arquivos .AppImage na pasta de downloads
+    echo "Deleting other .AppImage files..."
+    find "$pasta_downloads" -maxdepth 1 \( -iname "*.AppImage" -o -iname "*.appimage" \) -type f -not -name "${filename}" -delete
+    echo "Deletion complete!"
   fi
 fi
+
